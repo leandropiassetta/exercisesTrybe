e400b8425b6e3e94694253dd0f232f96bb534569
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-14 14:35:30 -0300

    exRefactored

--- Fundamentos-Web/BlOCO-4/Dia-1/exec04.js
+++ Fundamentos-Web/BlOCO-4/Dia-1/exec04.js
@@ -1,11 +1,15 @@
 /* 4- Make a program that, given a defined value in a variable, returns "positive" if this value is positive, "negative" if it is negative and "zero" otherwise.
 */
 
-let number = 10;
- if(number > 0){
-     console.log("positive");
- } else if (number < 0){
-     console.log("negative");
- }else{
-     console.log("zero");
- }
+const number = 20
+
+function positiveOrNegative(number) {
+    if(typeof number !== Number) {
+        Number(number)
+    }
+    if(number > 0) return 'positive';
+    if(number < 0) return 'negative';
+    if(number == 0) return 'zero';
+}
+
+console.log(positiveOrNegative('-5'))
