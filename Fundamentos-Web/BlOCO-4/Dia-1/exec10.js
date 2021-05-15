29874355f7aeebbcb8d9b3cfecce40b0acd0ceb5
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-15 17:14:57 -0300

    refactored code

--- Fundamentos-Web/BlOCO-4/Dia-1/exec10.js
+++ Fundamentos-Web/BlOCO-4/Dia-1/exec10.js
@@ -8,17 +8,20 @@ profit = valueSale - valueCostTotal (profit of a product)
 
 */
 
-let cost = 5;
-let value = 10;
-let product = 1000;
-let totalCost = 5 * 0.2 + 5;
-
-let profit = (value - totalCost) * product;
-
-if(profit < 0){
-    console.log("Error");
+const costProduct = 0;
+const saleValue = 4 ;
+const itensProduct = 1000;
+function profitFromSale(costProduct, saleValue) {
+    const totalCost = (costProduct * 0.2) + costProduct;
+    const totalProfit = (saleValue - totalCost) * itensProduct;
+    if(totalProfit <= 0) {
+        console.log('you no had profit, please! Dedicate yourself more to sales.')
+    } else {
+        console.log(totalProfit)
+    }
 }
 
+profitFromSale(costProduct, saleValue)
 
 
 
--- Fundamentos-Web/BlOCO-4/Dia-1/exec11.js
+++ Fundamentos-Web/BlOCO-4/Dia-1/exec11.js
@@ -25,48 +25,55 @@ Result: R$ 2.612,55.
 Tip: how about identifying the rates with explanatory name variables?
 */
 
-let grossSalary = 3000;
-let inss;// National Institute of Social Security.
-let ir;// Income Tax
-let basicSalary, salaryLiquid;
+const grossSalary = 3000;
 
+let inss; // National Institute of Social Security.
+function inssCalcule(grossSalary) {
+    if(grossSalary <= 0){
+        inss = 0;
+    }
+    else if(grossSalary <= 1556.94) {
+        inss = grossSalary * 0.08; // 8% for INSS
+    }
+    else if (grossSalary >= 1556.95 && grossSalary <= 2594.92) {
+        inss = grossSalary * 0.09; // 9% for INSS
+    }
+    else if (grossSalary >= 2594.93 && grossSalary <= 5189.82) {
+        inss = grossSalary * 0.11; // 11% for INSS
+    }    
+    else {
+        inss = 570.88; // maximum INSS
+    }
+    return inss;
+}
 
-if(grossSalary <= 0){
-    console.log ("Invalid salary, please retype it")
-}
-else if(grossSalary <= 1556.94){
-    inss = grossSalary * 0.08; // 8% for INSS
-}
-else if (grossSalary >= 1556.95 && grossSalary <= 2594.92){
-    inss = grossSalary * 0.09; // 9% for INSS
-}
-else if (grossSalary >= 2594.93 && grossSalary <= 5189.82){
-    inss = grossSalary * 0.11; // 11% for INSS
-}    
-else{
-    inss = 570.88; // maximum INSS
-}
-
-basicSalary = grossSalary - inss;
-
-//----------------------------------------------------------
+const basicSalary = inssCalcule(grossSalary) > 0
+    ? grossSalary - inssCalcule(grossSalary) : "Invalid salary, please retype it";
 
 //Calculating IR(Income tax)
 
-if(basicSalary > 1903.98 && basicSalary <= 2826.65){
-    ir = basicSalary * 0.075 - 142.80; // 7.5% for IR
+let ir; // Income Tax
+function irCalcule(basicSalary) {
+    if(basicSalary > 1903.98 && basicSalary <= 2826.65) {
+        ir = (basicSalary * 0.075) - 142.80; // 7.5% for IR
+    }
+    
+    else if(basicSalary >= 2826.66 && basicSalary <= 3751.05) {
+        ir = (basicSalary * 0.15) - 354.80; // 15% for IR
+    }
+    else if(basicSalary >= 3751.06 && basicSalary <= 4664.68) {
+        ir = (basicSalary * 0.225) - 636.13; // 22,5% for IR
+    }
+    else{
+        ir = (basicSalary * 0.275) - 869.36; // 27,5 for maximum IR
+    }
+    return ir;
 }
 
-else if(basicSalary >= 2826.66 && basicSalary <= 3751.05){
-    ir = basicSalary * 0.15 - 354.80; // 15% for IR
-}
-else if(basicSalary >= 3751.06 && basicSalary <= 4664.68){
-    ir = basicSalary * 0.225 - 636.13; // 22,5% for IR
-}
-else{
-    ir = basicSalary * 0.275 - 869.36; // 27,5 for maximum IR
+const salaryLiquid = () => {
+    const salary = basicSalary - irCalcule(basicSalary);
+    return console.log(`Your salary liquid is: ${salary.toFixed(2)}`)
 }
 
-salaryLiquid = basicSalary - ir;
+salaryLiquid()
 
-console.log(salaryLiquid)
