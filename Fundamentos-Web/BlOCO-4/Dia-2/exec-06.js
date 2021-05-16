a08da20da995a18bd39d8d69ff76393ce0ea5f1f
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-16 17:41:17 -0300

    exLogica

--- Fundamentos-Web/BlOCO-4/Dia-2/exec-06.js
+++ Fundamentos-Web/BlOCO-4/Dia-2/exec-06.js
@@ -1,7 +1,18 @@
 //6 - Find out how many odd values there are in the array and print the result. If there are none, print the message: "no odd value found".//
 
-const numbers = [- 4, 5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 101, 103];
+const numbers = [- 4, 5, '9', 3, 19, 70, 8, 100, 2, 35, 27, 101, 'l', 103, 2, 3];
+// const numbers =[]
 
-const oddNumbersLength = (numbers) => numbers.filter(num => num % 2 !== 0).length
+const oddNumbersLength = (numbers) => {
+  const sizeOddNumbers =  numbers.filter(num => {
+    return isNaN(num) ? false : num % 2 !== 0
+  }).length;
 
-console.log(oddNumbersLength(numbers))
+  if(sizeOddNumbers) {
+    console.log(sizeOddNumbers);
+  } else {
+    console.log('no oddNumbers value found');
+  }
+}
+
+oddNumbersLength(numbers)
