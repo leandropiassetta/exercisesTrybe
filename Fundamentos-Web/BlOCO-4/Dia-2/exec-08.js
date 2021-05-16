c9e1c20ae9234fa8d2fc05a53c3a1644e7c9498b
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-16 18:09:48 -0300

    exLogicaResolvido

--- Fundamentos-Web/BlOCO-4/Dia-2/exec-08.js
+++ Fundamentos-Web/BlOCO-4/Dia-2/exec-08.js
@@ -1,1 +1,11 @@
 // 8 - Using for, create an array from 1 to 25 and print the result. //
+
+const array_1_To_25 = () => {
+  const numbers = [];
+  for(i = 1; i <= 25; i += 1) {
+    numbers.push(i)
+  }
+  console.log(numbers)
+}
+
+array_1_To_25()
