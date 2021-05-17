03e996ee3f2af8e505b1ccfcc7bb33b23c27fad1
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-17 20:06:23 -0300

    Exercicio de Logica Resolvido

---
+++ Fundamentos-Web/BlOCO-4/Dia-2/bonus-01.js
@@ -0,0 +1,16 @@
+// 1. Sort the array numbers in ascending order and print their values. //
+
+//Exercise 1:
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = 0; j < numbers.length - i; j++) {
+    if (numbers[j] > numbers[j + 1]) {
+      let aux = numbers[j] ;
+      numbers[j] = numbers[j + 1];
+      numbers[j + 1] = aux;
+    }
+  }
+}
+
+console.log(numbers.join(', '));
