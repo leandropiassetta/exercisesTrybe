c06cfa408269c8f52628e85dc0ff5abc55d982ce
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-18 10:59:22 -0300

    ex Bonus 2 e 3 Resolvido

--- Fundamentos-Web/BlOCO-4/Dia-2/bonus-02.js
+++ Fundamentos-Web/BlOCO-4/Dia-2/bonus-02.js
@@ -1,3 +1,17 @@
 // 2. Order the array numbers in descending order and print their values. //
 
 // Exercises 2:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = 0; j < numbers.length - i; j++) {
+    if (numbers[j] < numbers[j + 1]) {
+      let aux = numbers[j] ;
+      numbers[j] = numbers[j + 1];
+      numbers[j + 1] = aux;
+    }
+  }
+}
+
+console.log(numbers.join(' '));
--- Fundamentos-Web/BlOCO-4/Dia-2/bonus-03.js
+++ Fundamentos-Web/BlOCO-4/Dia-2/bonus-03.js
@@ -1,3 +1,16 @@
 // 3. Now create a new array from the numbers array, without losing it. Each value of the new array must be equal to the corresponding value in the array numbers multiplied by the following. For example: the first value of the new array should be 45, as it is the multiplication of 5 (first value) and 9 (next value). The second value of the new array should be 27, since it is the multiplication of 9 (second value) and 3 (next value), and so on. If there is no next value, the multiplication must be done by 2. Do this using the for and the push method. The result should be the array below.//
 
 // Exercises 3:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let newNumbers = [];
+for (let i = 0; i < numbers.length; i++) {
+  if (i + 1 < numbers.length) {
+    newNumbers.push(numbers[i] * numbers[i + 1]);
+  } else {
+    newNumbers.push(numbers[i] * 2);
+  }
+}
+
+console.log(newNumbers.join(' '));
