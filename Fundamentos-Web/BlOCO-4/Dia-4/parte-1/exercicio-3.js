df5b6017cf8e9b7d9523a026eea3117137b9f15b
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-20 16:26:06 -0300

    Resolvendo exercicios de Logica envolvendo Objetos e Funcoes

---
+++ Fundamentos-Web/BlOCO-4/Dia-4/parte-1/exercicio-3.js
@@ -0,0 +1,27 @@
+/*Make a for/in that shows all the keys of the object.*/
+const  infoPersonage = {
+  personage: 'Margarida',
+  origin: 'Pato Donald',
+  note: 'Girlfriend of the main personage in Donald Duck comics.',
+  recurrent: 'Yes'
+};
+
+// The value expected in the console:
+
+/*
+    personage
+    origin
+    note
+    recurrent
+*/
+
+// Exercise 3:
+
+const showAllKeys = (object) => {
+  for(key in object) {
+    console.log(key)
+  }
+}
+
+showAllKeys(infoPersonage)
+
