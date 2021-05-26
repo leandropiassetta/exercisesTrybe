2335b273541d5642bc94bab746eec52874758ed5
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-26 14:50:53 -0300

    JavaScript| Objetos e Funçoes

---
+++ Fundamentos-Web/BlOCO-4/Dia-4/parte-2/exercicio-5.js
@@ -0,0 +1,20 @@
+// Exercise 5: 
+
+const numbers = [2, 3, 2, 5, 8, 2, 3];
+
+// The value expected is -> 2
+
+function numberWithMoreRepetitions(arr) {
+  const counts = arr.reduce((acc, cur) => {
+    if (!acc[cur]) acc[cur] = 0;
+    acc[cur] += 1;
+    return acc;
+  }, {});
+
+  const entries = Object.entries(counts);
+  entries.sort((countA, countB) => countB[1] - countA[1]);
+
+  return entries[0][0];
+}
+
+console.log(numberWithMoreRepetitions(numbers))
