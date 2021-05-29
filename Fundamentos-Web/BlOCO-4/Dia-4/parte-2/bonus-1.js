a9f0b8d27e791d362620e7dda44c615574f9f913
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-29 14:41:09 -0300

    JavaScript| Objetos e Funcoes

---
+++ Fundamentos-Web/BlOCO-4/Dia-4/parte-2/bonus-1.js
@@ -0,0 +1,46 @@
+const assert = require('assert');
+
+function romanDigitToDecimal(romanNumber) {
+  const dict = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+  return dict[romanNumber];
+}
+
+function combineRomanNumbers(numberA, numberB) {
+  const decimalA = romanDigitToDecimal(numberA);
+  const decimalB = romanDigitToDecimal(numberB);
+  if (decimalA >= decimalB) return decimalA;
+  return -decimalA;
+}
+
+function romanToDecimal(romanNumber) {
+  const lastRomanNumber = romanNumber[romanNumber.length - 1]
+  let result = romanDigitToDecimal(lastRomanNumber);
+  for (let index = romanNumber.length - 1; index > 0; index -= 1) {
+    const A = romanNumber[index - 1];
+    const B = romanNumber[index];
+    result += combineRomanNumbers(A, B);
+  }
+  return result;
+}
+
+console.log(romanToDecimal('XXVIV'));
+
+{
+  const romanNumber = 'CDXLIX';
+  const expect = 449;
+  assert.strictEqual(romanToDecimal(romanNumber), expect);
+}
+
+{
+  const romanNumber = 'IV';
+  const expect = 4;
+  assert.strictEqual(romanToDecimal(romanNumber), expect);
+}
